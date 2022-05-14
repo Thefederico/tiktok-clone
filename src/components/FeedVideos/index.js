830fb77816a8dd6241b6d32cac102a7ac3003868
Thefederico <federico_upegui.dev@outlook.com>
2022-05-14 18:56:24 -0500

    feat(Root): building feed videos

---
+++ src/components/FeedVideos/index.js
@@ -0,0 +1,16 @@
+const VIDEOS = [
+  {
+    author: "howridiulous",
+    decription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    src: "https://v16-webapp.tiktok.com/e56b666e41f82f7ea244d7b76c89a7e3/62807c69/video/tos/alisg/tos-alisg-pve-0037c001/7af20f18440a49459b2aada340b6920e/?a=1988&br=3430&bt=1715&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=eXd.6HHMMyq8ZFMolwe2NSd3yl7Gb&l=202205142206290102231230341AD574FD&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2Y6aWk6ZmtqOzMzODczNEApZWRmOWg6NmRkN2k7NzNnOGc2ZGJecjRnamlgLS1kMS1zczRgYmI0MzJgYTVjLWMzXi06Yw%3D%3D&vl=&vr=",
+  },
+  {
+    author: "espn",
+    decription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    src: "https://v16-webapp.tiktok.com/578149524a4b8db35831032f8c829851/62808779/video/tos/maliva/tos-maliva-ve-0068c799-us/13b339664aff4b5583845f744a3dfff8/?a=1988&br=3420&bt=1710&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=eXd.6HHMMyq8Z-K3lwe2Nitwyl7Gb&l=2022051422540401022306617120DF8793&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzxqd2k6Zm1nOzMzZzczNEApNzxlNzc5ODs0NztmZTdoaWdoLzNycjQwamhgLS1kMS9zczUwLTJfLjExNl8tXi00YmI6Yw%3D%3D&vl=&vr=",
+  },
+];
+
+export default function FeedVideos() {}
